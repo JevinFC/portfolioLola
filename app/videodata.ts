e8cf9077886906a6848a977e0a2ef7b97876189c
JevinFC <k.machado06@hotmail.com>
2026-06-17 00:47:52 +0200

    Update video URLs in videodata.ts to use Cloudinary links and fix title formatting

--- app/videodata.ts
+++ app/videodata.ts
@@ -2,34 +2,33 @@ const videos = [
   {
     title: "Vidéo 1 - Découvertes Scène nationale",
     slug: "video-1",
-    url: "/videosHome/DecouvertesHAG2526.mp4",
+    url: "https://res.cloudinary.com/dkwxhd6ck/video/upload/v1781640901/2DecouvertesHAG2526_hwv1jf.mp4",
     description: "Vidéo réalisée dans le cadre de la programmation de la Scène nationale de Blois."
   },
   {
     title: "Vidéo 2 - Scène nationale X Chato'do",
     slug: "video-2",
-    url: "/videosHome/SceneNationaleXChatoDo.mp4",
+    url: "https://res.cloudinary.com/dkwxhd6ck/video/upload/v1781640713/SceneNationaleXChatoDo_l9awwq.mp4",
     description: "Vidéo réalisée dans le cadre de la collaboration entre la Scène nationale de Blois et le Chato'do."
   },
   {
     title: "Vidéo 3 - Récapitulatif Génération Climat",
     slug: "video-3",
-    url: "/photosProjectSlugs/photosHAG/RecapGenerationClimat.mp4",
+    url: "https://res.cloudinary.com/dkwxhd6ck/video/upload/v1781640759/RecapGenerationClimat_xy0jya.mp4",
     description: "Récapitulatif d'une vidéo réalisée pour Génération Climat"
   },
   {
     title: "Vidéo 4 - Teaser Halle Aux Grains",
     slug: "video-4",
-    url: "/photosProjectSlugs/photosHAG/TeaserHalleAuxGrains2526.mp4",
+    url: "https://res.cloudinary.com/dkwxhd6ck/video/upload/v1781640740/TeaserHalleAuxGrains2526_klb2zx.mp4",
     description: "Teaser de la Halle Aux Grains"
   },
   {
-    title: "Vidéo 5 -Vidéo voeux 2026",
+    title: "Vidéo 5 - Vidéo voeux 2026",
     slug: "video-5",
-    url: "/photosProjectSlugs/photosHAG/Vidéo voeux 2026.mp4",
+    url: "https://res.cloudinary.com/dkwxhd6ck/video/upload/v1781640726/Vide%CC%81o_voeux_2026_mxfho0.mp4",
     description: "Vidéo de voeux de la Halle aux Grains"
   },
-  
 ];
 
 export default videos;
